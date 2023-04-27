d2c42bbcec4fcdf99a74114db89e5abe13101033
technologicaljerry <cserajatverma@gmail.com>
2023-04-27 23:27:21 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('Hello, Welcome to fstify server application');
+import fastify from 'fastify';
+
+const server = fastify()
+
+const PORT = 3000;
+
+async function main() {
+    try {
+        await server.listen(PORT, '0.0.0.0');
+        console.log(`Fastify server running on localhost post: ${PORT}`);
+
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+main();
