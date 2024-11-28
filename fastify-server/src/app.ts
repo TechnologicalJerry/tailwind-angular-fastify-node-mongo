24a11874ed54fba4c7d11ec1727d3f7a495368c0
technologicaljerry <cserajatverma@gmail.com>
2024-11-28 21:41:49 +0530

    Create app.ts

---
+++ fastify-server/src/app.ts
@@ -0,0 +1,27 @@
+import fastify from 'fastify';
+import userRoutes from './modules/users/user.routes';
+
+const server = fastify()
+
+server.get('/heakthcheck', async function () {
+    return {
+        status: "Health OK!"
+    }
+})
+
+const PORT = 3000;
+
+async function main() {
+    server.register(userRoutes, { prefix: 'api/isers' });
+
+    try {
+        await server.listen(PORT, '0.0.0.0');
+        console.log(`Fastify server running on localhost post: ${PORT}`);
+
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+main();
