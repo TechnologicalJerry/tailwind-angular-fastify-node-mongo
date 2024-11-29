a0db81664e413dba17a85196e4e910325c6a28e2
technologicaljerry <cserajatverma@gmail.com>
2024-11-29 13:10:39 +0530

    Delete user.routes.ts

--- src/modules/users/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import astify, { FastifyInstance } from "fastify";
-import { registerUserHandler } from './user.controllers'
-
-async function userRoutes(server: FastifyInstance) {
-
-    server.post('/', registerUserHandler);
-}
-
-export default userRoutes;
